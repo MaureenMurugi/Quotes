369db6a66a08bfd7a64d6f17771c6fa6dd61ec4c
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-02-07 09:27:20 +0300

    add the forms module import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { TimeCountPipe } from './time-count.pipe';
+
 
 @NgModule({
   declarations: [
@@ -15,10 +19,12 @@ import { TimeCountPipe } from './time-count.pipe';
     QuoteFormComponent,
     QuoteDetailComponent,
     TimeCountPipe
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
